5b8bf45f7afbce92ba5e616ed39dc8cd16920c1b
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-17 20:01:47 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,20 @@
 def consolidate_cart(cart)
-  # code here
+ newHash = {}
+  cart.each do |food, info|
+      food.each do |a,b|
+        if newHash[a]== nil
+          newHash[a]={price: b[:price], clearance: b[:clearance], count: 1}
+        else
+          newHash[a][:count]+=1
+        end
+      end
+  end
+  return newHash
 end
 
+
 def apply_coupons(cart, coupons)
-  # code here
+  
 end
 
 def apply_clearance(cart)
